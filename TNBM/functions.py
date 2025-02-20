096b901e999ff1c54c14b21feb33e355b1f3917f
Mattia Robbiano <mattia@DebianMattia.homenet.telecomitalia.it>
2025-02-20 11:03:49 +0100

    refactoring

---
+++ TNBM/functions.py
@@ -0,0 +1,136 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_bars_and_stripes(n):
+
+    bitstrings = [list(np.binary_repr(i, n))[::-1] for i in range(2**n)]
+    bitstrings = np.array(bitstrings, dtype=int)
+    
+    stripes = bitstrings.copy()
+    stripes = np.repeat(stripes, n, 0)
+    stripes = stripes.reshape(2**n, n * n)
+
+    bars = bitstrings.copy()
+    bars = bars.reshape(2**n * n, 1)
+    bars = np.repeat(bars, n, 1)
+    bars = bars.reshape(2**n, n * n)
+
+    return np.vstack((stripes[0 : stripes.shape[0] - 1], bars[1 : bars.shape[0]]))
+
+def print_bitstring_distribution(data):
+#
+# Simple function to lot histogram of occurence of bitstring in the dataset over all possible bitstring of that dimension
+#
+    n = data.shape[1]
+    print(data.shape)
+    bitstrings = []
+    nums = []
+    for d in data:
+        bitstrings += ["".join(str(int(i)) for i in d)]
+        nums += [int(bitstrings[-1], 2)]
+        probs = np.zeros(2**n)
+        probs[nums] = 1 / len(data)
+        
+    plt.figure(figsize=(12, 5))
+    plt.bar(np.arange(2**n), probs, width=2.0, label=r"$\pi(x)$")
+    plt.xticks(nums, bitstrings, rotation=80)
+    plt.xlabel("Samples")
+    plt.ylabel("Prob. Distribution")
+    plt.legend(loc="upper right")
+    plt.subplots_adjust(bottom=0.3)
+    plt.show()
+
+def get_distribution(data):
+    #
+    # Simple function to plot discrete probability distribution of a bitstring dataset over all the possible bitstrings
+    # with the same dimension.
+    #
+    bitstrings = []
+    nums = []
+    for d in data:
+        bitstrings += ["".join(str(int(i)) for i in d)]
+        nums += [int(bitstrings[-1], 2)]
+    py = np.zeros(2**SAMPLE_BITSTRING_DIMENSION)
+    py[nums] = 1 / len(data)
+    return py
+
+class ADAM:
+    def __init__(
+        self,
+        n_param,
+        tol: float = 1e-6,
+        lr: float = 1e-1,
+        beta_1: float = 0.9,
+        beta_2: float = 0.99,
+        noise_factor: float = 1e-8,
+        eps: float = 1e-10,
+        amsgrad: bool = False,
+    ) -> None:
+
+        self._tol = tol
+        self._lr = lr
+        self._beta_1 = beta_1
+        self._beta_2 = beta_2
+        self._noise_factor = noise_factor
+        self._eps = eps
+        self._amsgrad = amsgrad
+
+
+        self._t = 1
+        self._m = np.zeros((n_param))
+        self._v = np.zeros((n_param))
+        if self._amsgrad:
+            self._v_eff = np.zeros((n_param))
+
+    def update(self,params,derivative):
+
+        self._m = self._beta_1 * self._m + (1 - self._beta_1) * derivative
+        self._v = self._beta_2 * self._v + (1 - self._beta_2) * derivative * derivative
+        lr_eff = self._lr * np.sqrt(1 - self._beta_2**self._t) / (1 - self._beta_1**self._t)
+        if not self._amsgrad:
+            params_new = params - lr_eff * self._m.flatten() / (
+                    np.sqrt(self._v.flatten()) + self._noise_factor
+                )
+        else:
+            self._v_eff = np.maximum(self._v_eff, self._v)
+            params_new = params - lr_eff * self._m.flatten() / (
+                    np.sqrt(self._v_eff.flatten()) + self._noise_factor
+                )
+        self._t +=1
+        return params_new
+
+class MMD:
+#
+# Define a MMD object, initialized once given the points on which we are evaluating exected value of kernel function
+# and sigmas.
+#
+# METHODS:
+#    
+# __init__ ::
+#    
+#    scales: array of sigmas of the kernel functions we want to sum over
+#    space:  array of points at which probability distributions will be evaluated when calculating expectation
+#    value
+#   
+#    sq_dists: (space[:m None] - space[None, :]) the two 1d arrays are casted in a column and row matrices
+#    subtracting, we are creating a matrix of pairwise distances (each squared by **2).
+#
+# k_expval ::
+#    
+#    returns expected value of K(x,y) over the distributions px, py, meaning we will extract x from px and
+#    y from py
+#
+# __call__ :: returns the loss value, calculated with reduced expression, see google doc in readme for references    
+#        
+    def __init__(self, scales, space):
+        gammas = 1 / (2 * (scales**2))
+        sq_dists = jnp.abs(space[:, None] - space[None, :]) ** 2
+        self.K = sum(jnp.exp(-gamma * sq_dists) for gamma in gammas) / len(scales)
+        self.scales = scales
+
+    def k_expval(self, px, py):
+        return px @ self.K @ py
+
+    def __call__(self, px, py):
+        pxy = px - py
+        return self.k_expval(pxy, pxy)
--- TNBM/main_tn.py
+++ TNBM/main_tn.py
@@ -1,3 +1,14 @@
+#
+# DA FARE: 
+# Ricorda! E' un modello implicito, quindi non conosciamo la distribuzione target (nell'esempio toy facciamo finta di non conoscerla)
+# Bisogna aggiustare la MMD. Al momento è come quella di di Oriel con exact True, ci serve l'altra, cioè quella per i sample.
+# Il training non si può fare con autograd così com'è. Bisogna fare un training loop e fare i gradienti con parameter shift.
+# Bisogna capire come si fa a rendere la bond dimension una roba addestrabile
+# 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import json
 import numpy as np
 import math
@@ -10,26 +21,20 @@ import quimb.tensor as qtn
 from  QCBM.born_machine import QCBM, MMD
 from fun.dataset import get_bars_and_stripes
 from fun.plotting import *
-from sampling import perfect_sampling
 
-
+#
 # Importing configuration file
+# DEVICE = "cpu" or "gpu" (strings)
+#
 with open("parameters.json", "r") as f:
     config = json.load(f)
 SAMPLE_BITSTRING_DIMENSION = config['SAMPLE_BITSTRING_DIMENSION']
 PRINT_TARGET_PDF = config['PRINT_TARGET_PDF']
-DEVICE = config['DEVICE'] #cpu or gpu string format
-
-
+DEVICE = config['DEVICE']
+EPOCHS = config['EPOCHS']
 jax.config.update("jax_platform_name", DEVICE)
-
-
-# Setting circuit parameters
 np.random.seed(42)
 n_qubits = SAMPLE_BITSTRING_DIMENSION
-
-
-# Output configuration parameters to user
 print()
 print("Configuration:")
 print(f"bitsting samples dimension: {SAMPLE_BITSTRING_DIMENSION}")
@@ -39,65 +44,79 @@ print(f"number of different samples:{2**(int(math.sqrt(SAMPLE_BITSTRING_DIMENSIO
 print(f"print target probability distribution: {PRINT_TARGET_PDF}")
 print(f"Using: {jax.devices()}")
 print()
-  
-
-def get_distribution(data):
-    bitstrings = []
-    nums = []
-    for d in data:
-        bitstrings += ["".join(str(int(i)) for i in d)]
-        nums += [int(bitstrings[-1], 2)]
-    py = np.zeros(2**SAMPLE_BITSTRING_DIMENSION)
-    py[nums] = 1 / len(data)
-    return py
 
 
 def main():
-    # Building dataset
+    # 
+    # Building dataset. We will then provide the model one data at the time and calculate MMD for each sample
+    #
     data = get_bars_and_stripes(int(math.sqrt(SAMPLE_BITSTRING_DIMENSION)))
     if PRINT_TARGET_PDF == True:
         print_bitstring_distribution(data)
-    py = get_distribution(data)
 
-
-    # Initializing MPS
-    number_open_index = 8    # 8 blocchi di gate
-    bond_dimension =   2**8  # rappresenta esattamente stati fino a 16 qubit
+    #
+    # MPS initialization. The number of open indexes controls the number of nodes in the MPS, and thus the number of 
+    # gate blocks. Bond_dimension represent maximum bond dimension, that controls the expressivity of the model.
+    # bond_dimension = 2^(D/2) where N is the number of qubits of the largest state exactly representable. 
+    #
+    number_open_index = 2
+    bond_dimension =   2**8
     psi = qtn.MPS_rand_state(L=number_open_index, bond_dim=bond_dimension)
 
-    
+    #
     # Building Born Machine and MMD
+    #
     bandwidth = jnp.array([0.25, 0.5, 1])
     space = jnp.arange(2**n_qubits)
     mmd = MMD(bandwidth, space)
 
-
-    # Optimization
-    def sample_tn(psi):
-        samples = []
-        for i in range(512):
-            samples.append(perfect_sampling(psi))
-        return samples
-
-    def loss_fn(psi, py, mmd):
-        samples = sample_tn(psi)
-        px = get_distribution(samples)
-        loss = mmd(px,py)
-        return loss
+    #
+    # OPTIMIZATION
+    # We have to understand which are our parameters. We have to give a fitting format to data to be given to tn.
+    # At every loop of r we change learning rate for convergence.
+    # At every epoch we wanto to compute gradients to update the MPS. We calculate the MMD loss inside the gradient computation.
+    #
+    rep = 10
+    lr0 = 0.1
     
-    #print(f"Initial loss value {loss_fn(psi,py,mmd)}")
-    print()
-    print()
-    print()
-    tnopt = qtn.TNOptimizer(
-        psi,
-        loss_fn = loss_fn,
-        loss_kwargs={"py": py,"mmd":mmd},
-        optimizer="adam",
-        autodiff_backend= "jax"
-    )
-    psi_opt = tnopt.optimize(10)
+    np.random.seed(seed)
+    parameters = np.random.normal(0, np.pi, size  = n_param)
+    parameters[1:] = 0
+    optimizer = ADAM(n_param, lr = lr0)
+    seed = 12+42 + 53*(1+r)
 
+    for ep in range(EPOCHS):
+
+        lr = lr0*np.exp(-0.005*ep)
+        lr = max(10**-6,lr)
+        optimizer._lr = lr
+        np.random.shuffle(target)
+        target_train = target[:batch_size,...]
+
+        median_grad = []
+        global intermediary_tv
+        intermediary_tv = []
+
+        def loss(x):
+            #
+            # MMD loss function #
+            #
+            state_raw = qulacs.QuantumState(qubits)
+            state_raw = ansatz(state_raw, x)
+            samples = compute_samples(state_raw, exact = exact, n_shots=n_shots,
+                                    n_qubits=qubits, projectors=projectors,values=values)
+            loss =  MMD(samples,target_train,signal,exact)
+            loss = np.mean(loss)
+
+            return float(loss)
+
+        for _ in range(grad_batch):
+
+            gradients = compute_gradient(ansatz, parameters, target_train, qubits, n_shots,grad_loss_function, signal = signal, exact = exact, values = values)
+            median_grad.append(gradients)
+
+        temp_grad = np.mean(np.mean(np.array(median_grad), axis=0), axis=-1).reshape(-1)
+        parameters = optimizer.update(parameters,temp_grad)
 
 if __name__ == "__main__":
     main()
