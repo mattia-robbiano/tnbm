bac8085ad9d7efb43695060ac389ba3d1c6ef544
Mattia Robbiano <mrobbian@olqti-gpu-04.cern.ch>
2025-04-07 18:21:35 +0200

    training script

--- src/main.py
+++ src/main.py
@@ -10,36 +10,25 @@ from dataset_utils import bars_and_stripes, hypertn_from_data, plot_binary_data
 
 loss_list = []
 def callback_fn(tnopt):
-    """Callback to compute the actual KL divergence loss."""
-    psi = tnopt.get_tn_opt()
-    htn_data = tnopt.loss_constants['htn_data']
-    
-    # Contract overlap tn
-    loss = (psi | htn_data).contract(output_inds = ['hyper'], 
+    g = jax.grad(tnopt.loss_fn)(tnopt.get_tn_opt(), **tnopt.loss_constants)
+    print("Gradient:", g)
+
+def nnl_loss(tn, htn_data):
+    """
+    Calculate the KL divergence between the MPS and the dataset.
+    """
+    loss = (tn | htn_data).contract(output_inds = ['hyper'], 
                                      optimize = 'auto-hq').data
     
-    # Get probabilities (Born's rule)
-    q = jnp.abs(loss) ** 2
-    
-    # Normalize so that the dataset probability sums to one
-    # and is comparable with the p vector below so that they are both 
-    # vector of probabilities summing to one
-    q = q / q.sum() 
-    
-    # Uniform probability over the samples
-    p = jnp.ones(len(q)) / len(q)
-    
-    kl_div = jnp.sum(p * jnp.log(p / q)).tolist()
-    
-    loss_list.append(kl_div) 
+    return -1. * jnp.mean( jnp.log( jnp.abs(loss)**2 ) )
 
-def mmd_loss(psi, povm_tn, kernel_mpo, htn_data):
+def mmd_loss(tn, povm_tn, kernel_mpo, htn_data):
     
-    num_sites = psi.L
+    num_sites = tn.L
 
-    probs_tn = qtn.TensorNetwork([psi.H,
+    probs_tn = qtn.TensorNetwork([tn.H,
                               povm_tn, 
-                              psi.reindex_sites('b{}')])
+                              tn.reindex_sites('b{}')])
     
     reindex_map = {f'o{i}': f'op{i}' for i in range(num_sites)}
     model_tn = qtn.TensorNetwork([probs_tn, 
@@ -56,7 +45,9 @@ def mmd_loss(psi, povm_tn, kernel_mpo, htn_data):
                                     kernel_mpo,
                                     htn_data.reindex(reindex_map_op)])
     
-    return model_tn.contract(output_inds=[], optimize='auto-hq') - 2*overlap_data_tn.contract(output_inds=[], optimize='auto-hq') + data_tn.contract(output_inds=[], optimize='auto-hq')
+    mmd = model_tn.contract(output_inds=[], optimize='auto-hq') - 2*overlap_data_tn.contract(output_inds=[], optimize='auto-hq') + data_tn.contract(output_inds=[], optimize='auto-hq')
+    
+    return mmd.real
 
 
 def main():
@@ -81,11 +72,6 @@ def main():
         tensors.append(t)
     povm_tn =  qtn.TensorNetwork(tensors)
 
-    # probabilities tn
-    probs_tn = qtn.TensorNetwork([psi.H,
-                                povm_tn, 
-                                psi.reindex_sites('b{}')])
-
     ######################## KERNEL ########################
     sigma = 0.5
     fact = 1/(2*sigma**2)
@@ -104,23 +90,29 @@ def main():
     kernel_mpo = kernel_mpo / kernel_mpo.norm()
 
     ######################## OPTIMIZER ########################
-    tnopt = qtn.TNOptimizer(psi, 
-                        loss_fn=mmd_loss,
+    tnopt = qtn.TNOptimizer(
+                        tn = psi, 
+                        loss_fn=nnl_loss,
+                        loss_constants={"htn_data":htn_data},
+                        #loss_constants={"povm_tn":povm_tn, "kernel_mpo":kernel_mpo, "htn_data":htn_data},
+                        loss_kwargs={},
                         norm_fn=lambda x: x / x.norm(),
-                        loss_constants={'htn_data': htn_data},
                         autodiff_backend='jax',
                         jit_fn=True,
-                        optimizer='adam')
-    psi_opt = tnopt.optimize(200)
+                        optimizer='adam',
+                        callback=callback_fn,
+                        )
+                    
+    psi_opt = tnopt.optimize(10)
 
     ######################## POSTPROCESSING ########################
-    num_samples = 25
-    samples = []
-    for b, p in psi_opt.sample(num_samples):
-        print(p)
-        samples.append(b)
-    samples = np.array(samples).reshape(num_samples, int(np.sqrt(num_sites)), int(np.sqrt(num_sites)))
-    plot_binary_data(samples)
+    # num_samples = 25
+    # samples = []
+    # for b, p in psi_opt.sample(num_samples):
+    #     print(p)
+    #     samples.append(b)
+    # samples = np.array(samples).reshape(num_samples, int(np.sqrt(num_sites)), int(np.sqrt(num_sites)))
+    # plot_binary_data(samples)
 
     fig, ax = tnopt.plot()
     fig.patch.set_facecolor('white')
