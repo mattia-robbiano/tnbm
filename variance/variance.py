0539b647101a2d7d2f4f33eb8dab099c22a2419c
Mattia Robbiano <mattiarobbiano@gmail.com>
2025-05-22 16:56:48 +0200

    refactor: variance rescaling for plot

--- variance/variance.py
+++ variance/variance.py
@@ -1,27 +1,20 @@
 import sys
 sys.path.append('../')
+import pickle
 import numpy as np
 import quimb.tensor as qtn
-
-import matplotlib.pyplot as plt
-#import scienceplots
 from functions.dataset_utils import bars_and_stripes, hypertn_from_data
 from functions.loss import nll_loss, mmd_loss
-#plt.style.use("science")
 
-# parameters
-chi_list = [2,10,30,50]
-num_sites_list = [2**2, 3**2, 4**2, 5**2, 6**2, 7**2, 8**2, 9**2]
-use_mmd_loss = True
 
-# computing variance
-variance_list = []
+chi_list =       [2, 4, 8, 16, 32]#, 64] sul cluster
+num_sites_list = [2**2, 3**2, 4**2, 5**2, 6**2]#, 7**2] sul cluster
+
+# Building the loss function tensors
+result = []
 for num_sites in num_sites_list:
-    print(f"Number of sites: {num_sites}")
 
-    p0 = np.array([[1,0],[0,0]]) 
-    p1 = np.array([[0,0],[0,1]])
-    comp_basis_povm = np.array([p0, p1])
+    comp_basis_povm = np.array([np.array([[1,0],[0,0]]), np.array([[0,0],[0,1]])])
     tensors = []
     for i in range(num_sites):
         t = qtn.Tensor(comp_basis_povm,inds=(f'o{i}', f'k{i}', f'b{i}'), tags = [f'I{i}', 'POVM'])
@@ -32,48 +25,32 @@ for num_sites in num_sites_list:
     sigma = 0.55
     fact = np.exp(-1. / (2 * sigma**2))
     kernel_mat = np.array([[1, fact],[fact, 1]])
-    kernel_mpo = qtn.MPO_product_operator([kernel_mat]*num_sites, 
-                                        upper_ind_id = 'o{}', 
-                                        lower_ind_id = 'op{}', 
-                                        tags = ['KERNEL'])
+    kernel_mpo = qtn.MPO_product_operator([kernel_mat]*num_sites,upper_ind_id = 'o{}',lower_ind_id = 'op{}',tags = ['KERNEL'])
     kernel_mpo = kernel_mpo / kernel_mpo.norm()
 
     bas = bars_and_stripes(int(np.sqrt(num_sites)), shuffle=True)
     num_samples = bas.shape[0]
     htn_freq = hypertn_from_data(data=bas)/num_samples
-    
-    variance_chi_list = []
+
     for chi in chi_list:
-        loss_list = []
-        contraction_method = 'opt' if num_sites > 25 or (num_sites == 25 and chi > 20) else 'auto-hq'
-        for _ in range(100):
-            psi = qtn.MPS_rand_state(num_sites, bond_dim=chi, dist='uniform')
-            if use_mmd_loss:
-                loss = mmd_loss(psi, povm_tn, kernel_mpo, htn_freq, contraction_method=contraction_method)
-            else:
-                loss = nll_loss(psi, htn_freq)
-            loss_list.append(loss)
-        variance= np.var(loss_list)
-        print(variance)
-        variance_chi_list.append(variance)
-    variance_list.append(variance_chi_list)
+        psi_list = [qtn.MPS_rand_state(num_sites, bond_dim=chi, dist='uniform') for _ in range(100)]
 
-# plotting variance_list
-plt.figure(figsize=(6, 6*6/8))
-for j, chi in enumerate(chi_list):
-    variance_for_chi = [variance_list[i][j] for i in range(len(num_sites_list))]
-    plt.plot(num_sites_list, variance_for_chi, marker='*', label=f'Bond Dim {chi}')
-plt.yscale('log')
+        mmd_list = [mmd_loss(psi, povm_tn, kernel_mpo, htn_freq, contraction_method='auto-hq') for psi in psi_list]
+        nll_list = [nll_loss(psi, htn_freq) for psi in psi_list]
 
-if use_mmd_loss == True: plt.title('Loss concentration MMD', fontsize=12)
-else: plt.title('Loss concentration MMD', fontsize=12)
-plt.xlabel('Number of Qubits', fontsize=12)
-plt.ylabel('Variance (Log Scale)', fontsize=12)
-plt.xticks(num_sites_list, fontsize=12)
-plt.yticks(fontsize=12)
-plt.grid(visible=False)
-plt.legend(fontsize=10, title_fontsize=10, ncol=1, bbox_to_anchor=(0.83, 0.87), loc='upper center')
-plt.tight_layout(pad=1.0)
-if use_mmd_loss == True:plt.savefig('loss_concentration_MMD.pdf')
-else: plt.savefig('loss_concentration_NLL.pdf')
-plt.show()
+        mmd_variance = np.var(mmd_list)
+        nll_variance = np.var(nll_list)
+
+        mmd_min = np.min(mmd_list)
+        nll_min = np.min(nll_list)
+
+        mmd_nlog_norm_var = - np.log(mmd_variance / mmd_min)
+        nll_nlog_norm_var = - np.log(nll_variance / nll_min)
+
+        print(f"num_sites: {num_sites}, chi: {chi}, mmd_log_norm_var: {mmd_nlog_norm_var}, nll_log_norm_var: {nll_nlog_norm_var}")
+
+        result.append({'num_sites': num_sites,'bond_dimension': chi,'loss_type': 'mmd', 'nlog_norm_var': mmd_nlog_norm_var,})
+        result.append({'num_sites': num_sites,'bond_dimension': chi,'loss_type': 'nll', 'nlog_norm_var': nll_nlog_norm_var,})
+
+    with open('result.pkl', 'wb') as f:
+        pickle.dump(result, f)
